fcad3c62479cd4e163fe33fb01719396919eff1d
hary473 <79105263+hary473@users.noreply.github.com>
2022-05-13 11:57:18 +0530

    Update version.tf

--- version.tf
+++ version.tf
@@ -1,1 +1,13 @@
-
+terraform {
+  required_version = "~> 0.14"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = "3.1.0"
+    }
+  }
+}
